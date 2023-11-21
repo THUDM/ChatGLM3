ce3db1bd9fa07862388f4731c929d70d320cbcf6
xxw <46432007+xxw1995@users.noreply.github.com>
2023-11-21 14:43:18 +0800

    Update concate_lora.py

--- finetune_model_for_agent/concate_lora.py
+++ finetune_model_for_agent/concate_lora.py
@@ -3,12 +3,12 @@ from peft import PeftModel, PeftConfig
 import torch
 import os
 
-model_name_or_path = "/opt/tiger/xuxuanwen/personal/LLaMA-Efficient-Tuning/meta-llama/llama-2-7b-extent/"
+model_name_or_path = "./model"
 
 tokenizer = AutoTokenizer.from_pretrained(model_name_or_path, trust_remote_code=True)
 model = AutoModel.from_pretrained(model_name_or_path, trust_remote_code=True, device_map='auto', torch_dtype=torch.bfloat16)#.half().cuda()
 
-peft_model_id = "/opt/tiger/xuxuanwen/personal/LLaMA-Efficient-Tuning/path_to_pt_checkpoint_2021-49_zh_head_000x_lora5000/checkpoint-39000"
+peft_model_id = "./output/checkpoint-39000"
 model = PeftModel.from_pretrained(model, peft_model_id)
 model = model.eval()
 
