b74bab7a72a065614266a0d0d36bf8ff15f4fa20
zR <2448370773@qq.com>
2024-02-06 21:56:23 +0800

    transformers>=4.37.2 for finetuning

--- finetune_demo/inference_hf.py
+++ finetune_demo/inference_hf.py
@@ -24,31 +24,30 @@ def _resolve_path(path: Union[str, Path]) -> Path:
     return Path(path).expanduser().resolve()
 
 
-def load_model_and_tokenizer(
-        model_dir: Union[str, Path], trust_remote_code: bool = True
-) -> tuple[ModelType, TokenizerType]:
+def load_model_and_tokenizer(model_dir: Union[str, Path]) -> tuple[ModelType, TokenizerType]:
     model_dir = _resolve_path(model_dir)
     if (model_dir / 'adapter_config.json').exists():
         model = AutoPeftModelForCausalLM.from_pretrained(
-            model_dir, trust_remote_code=trust_remote_code, device_map='auto'
+            model_dir, trust_remote_code=True, device_map='auto'
         )
         tokenizer_dir = model.peft_config['default'].base_model_name_or_path
     else:
         model = AutoModelForCausalLM.from_pretrained(
-            model_dir, trust_remote_code=trust_remote_code, device_map='auto'
+            model_dir, trust_remote_code=True, device_map='auto'
         )
         tokenizer_dir = model_dir
     tokenizer = AutoTokenizer.from_pretrained(
-        tokenizer_dir, trust_remote_code=trust_remote_code
+        tokenizer_dir, trust_remote_code=True
     )
     return model, tokenizer
+
 
 @app.command()
 def main(
         model_dir: Annotated[str, typer.Argument(help='')],
         prompt: Annotated[str, typer.Option(help='')],
 ):
-    model, tokenizer = load_model_and_tokenizer(model_dir, trust_remote_code=True)
+    model, tokenizer = load_model_and_tokenizer(model_dir)
     response, _ = model.chat(tokenizer, prompt)
     print(response)
 
