3ed9c3a9b0d78a0dde19f34a20b42a74322abadd
zR <2448370773@qq.com>
2024-06-16 00:04:56 +0800

    fix #1268

--- tools_using_demo/tool_register.py
+++ tools_using_demo/tool_register.py
@@ -40,7 +40,7 @@ def register_tool(func: callable):
     tool_def = {
         "name": tool_name,
         "description": tool_description,
-        "parameters": tool_params
+        "params": tool_params
     }
 
     print("[registered tool] " + pformat(tool_def))
